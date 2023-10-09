eeb955196a4a1c37b20bc6398d56f72063509e5d
NavinkumarVaradharaj <navincs27@gmail.com>
2023-10-09 12:35:22 +0530

    four elements added to product page class and interface

--- src/main/java/com/Limeroad_Quality_Central/pom/ProductPage.java
+++ src/main/java/com/Limeroad_Quality_Central/pom/ProductPage.java
@@ -1,5 +1,10 @@
 package com.Limeroad_Quality_Central.pom;
 
 public interface ProductPage {
-
+	
+	String shop_men = "//span[text()='SHOP MEN']";
+	String men_category = "men_category";
+	String sports_t_shirt = "(//a[contains(text(),'sports wear')]//following::a[text()='t-shirts'])[1]";
+	String trending_radio_button = "//label[@class='pl36 bs dB p2 pR srt ']//following::div[text()='trending']";
+	String brand_option = "//div[@data-a='brandid']";
 }
