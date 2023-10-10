9534f10586f2288b299a6da68a17eb4a329c9139
NavinkumarVaradharaj <navincs27@gmail.com>
2023-10-10 18:56:12 +0530

    added a sleep method to runner class

--- src/test/java/com/Limeroad_Quality_Central/runner/TestRunner.java
+++ src/test/java/com/Limeroad_Quality_Central/runner/TestRunner.java
@@ -2,4 +2,12 @@ package com.Limeroad_Quality_Central.runner;
 
 public class TestRunner {
 
+	public void sleep() {
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
