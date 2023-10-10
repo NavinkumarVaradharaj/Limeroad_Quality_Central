2ae5ac3b77b3e131e155be98d2f4e9d23208b8c5
sadasivan88 <sadasivan88@gmail.com>
2023-10-10 18:56:11 +0530

    added a wait method to test runner class

--- src/test/java/com/Limeroad_Quality_Central/runner/TestRunner.java
+++ src/test/java/com/Limeroad_Quality_Central/runner/TestRunner.java
@@ -1,5 +1,13 @@
 package com.Limeroad_Quality_Central.runner;
 
 public class TestRunner {
+	
+	public void waitPlease() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}}
 
 }
